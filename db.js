4a8104df3cb95469da1008f71541b3a0f05d45fc
Ankit Kumar <ankkrs@gmail.com>
2023-12-04 15:15:31 +0530

    ff

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose= require("mongoose");
 
-const connection = mongoose.connect(`mongodb+srv://nishant123:nishant123@cluster0.xgnitpc.mongodb.net/evalPracAnk?retryWrites=true&w=majority`)
+const connection = mongoose.connect(`mongodb+srv://ankitsimari:ankit123@cluster0.qb2hai2.mongodb.net/Ankitmock5try?retryWrites=true&w=majority`)
+
 
 module.exports={connection}
